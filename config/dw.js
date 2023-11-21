a0780c1117d2613de6ed7903703f4b2e4fcd6826
Paskal Novakov <p.novakov@astoundcommerce.com>
2023-11-21 15:56:24 +0200

    PWA-002:ADD: DW Env and process

---
+++ config/dw.js
@@ -0,0 +1,13 @@
+module.exports = {
+    SHORT_CODE: "kv7kzm78",
+    ORGANIZATION_ID: "f_ecom_zzte_053",
+    SITE_ID: "RefArch",
+    CLIENT_ID: "1d763261-6522-4913-9d52-5d947d3b94c4",
+    TENANT_ID: "zzte-053",
+    EINSTEIN_SITE_ID: "aaij-MobileFirst",
+    EINSTEIN_HOST: "https://api.cquotient.com",
+    EINSTEIN_ID: "1ea06c6e-c936-4324-bcf0-fada93f83bb1",
+    EINSTEIN_IS_PRODUCTION: false,
+    API_COMMERCE_CLOUD: "kv7kzm78.api.commercecloud.salesforce.com",
+    DX_COMMERCE_CLOUD: "zzte-053.dx.commercecloud.salesforce.com"
+}
